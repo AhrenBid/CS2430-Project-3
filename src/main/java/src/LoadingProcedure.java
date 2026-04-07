79109be8313c8ffc0c2531da71a95d900d18e9cc
ZakButters <buttezac003@gmail.com>
2026-04-06 20:03:53 -0600

    Update LoadingProcedure.java
    Refactor loadBruteForce method to return top 3 valid combinations of payloads under weight limit.

--- src/main/java/src/LoadingProcedure.java
+++ src/main/java/src/LoadingProcedure.java
@@ -41,11 +41,39 @@ public class LoadingProcedure
         return ratioBay;
     }
     
-    public CargoBay loadBruteForce()
-    {
-        CargoBay forceBay = new CargoBay(700);
+// Loads every possible combination of payloads and returns 
+// the top 3 combinations that are under the weight limit of 700
+    public ArrayList<CargoBay> loadBruteForce(Payload[] payloads) {
+        ArrayList<CargoBay> validSubsets = new ArrayList<CargoBay>();
+    
+    //loops through every possible combination of payloads using a bitmask
+        for (int mask = 0; mask < 4096; mask++) {
+            CargoBay forceBay = new CargoBay(700);
         
-        return forceBay;
+        // loops through all 12 payloads and adds the current iterationed ones to the cargo bay
+            for (int i = 0; i < 12; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    try {
+                    forceBay.addPayload(payloads[i]);
+                    } catch (Exception e) {
+                    // Skip past excpetion
+                    }
+                }
+            }
+        
+            // Only takes in valid combinations of payloads that are under 700
+            if (forceBay.getCargoWeight() <= 700) {
+            validSubsets.add(forceBay);
+        }
+    }
+
+        // Sorts valids subsets and adds the top 3 into array list top 3
+        Collections.sort(validSubsets, (a, b) -> b.getCargoValue() - a.getCargoValue());
+        ArrayList<CargoBay> top3 = new ArrayList<CargoBay>();
+        top3.add(validSubsets.get(0));
+        top3.add(validSubsets.get(1));
+        top3.add(validSubsets.get(2));
+        return top3;
     }
     
     public CargoBay loadDynamic()
