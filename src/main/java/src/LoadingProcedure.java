209c045e90c90384b12ce61d1f30171dd3650bcb
Andrewcclark <andrewchrisclark@gmail.com>
2026-04-06 20:03:19 -0600

    Update LoadingProcedure.java
    Add Andrew's Methods

--- src/main/java/src/LoadingProcedure.java
+++ src/main/java/src/LoadingProcedure.java
@@ -6,6 +6,8 @@ package src;
 
 import java.util.ArrayList;
 
+
+
 /**
  *
  * @author abidling
@@ -15,28 +17,118 @@ public class LoadingProcedure
     
     ArrayList<Payload> cargoCatalogue;
     
-    public LoadingProcedure(ArrayList<Payload> cargoCatagogue)
+    public LoadingProcedure(ArrayList<Payload> cargoCatalogue)
     {
         this.cargoCatalogue = new ArrayList<Payload>(cargoCatalogue);
     }
     
+    ///@author Andrew
+    ///Adds the highest rated items first
     public CargoBay loadHighestRating()
     {
         CargoBay ratingBay = new CargoBay(700);
+    	   
+        ArrayList<Payload> dupCatalogue = new ArrayList<>(cargoCatalogue);
+
+        while (true) {
+            Payload bestRating = null;
+            
+            //loops through catalogue to find item with the max ratio
+            for (int i = 0; i < dupCatalogue.size(); i++) {
+                Payload current = dupCatalogue.get(i);
+             //checks if weight keeps total under 700 and updates bestRatio if needed
+                if (current.getWeight() + ratingBay.getCargoWeight() <= 700) {
+                    if (bestRating == null || current.getRatio() > bestRating.getRatio()) {
+                        bestRating = current;
+                    }
+                }
+            }
+
+            if (bestRating == null) {
+                break;
+            }
+            //Prevents exception error since addPayload throws exception
+            try {
+                ratingBay.addPayload(bestRating);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            dupCatalogue.remove(bestRating);
+        }
         
         return ratingBay;
-    }
+	}	
     
-    public CargoBay loadLightest()
-    {
+    //@author Andrew
+    //Adds the lightest items first
+    public CargoBay loadLightest() {
         CargoBay lightestBay = new CargoBay(700);
-        
+        //Duplicate catalogue to prevent changes
+        ArrayList<Payload> dupCatalogue = new ArrayList<>(cargoCatalogue);
+
+        while (true) {
+            Payload lightest = null;
+            
+            //loops through catalogue to find item with the minimum weight
+            for (int i = 0; i < dupCatalogue.size(); i++) {
+                Payload current = dupCatalogue.get(i);
+             //checks if weight keeps total under 700 and updates lightest if needed
+                if (current.getWeight() + lightestBay.getCargoWeight() <= 700) {
+                    if (lightest == null || current.getWeight() < lightest.getWeight()) {
+                        lightest = current;
+                    }
+                }
+            }
+
+            if (lightest == null) {
+                break;
+            }
+            //Prevents exception error since addPayload throws exception
+            try {
+                lightestBay.addPayload(lightest);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            dupCatalogue.remove(lightest);
+        }
+
         return lightestBay;
     }
+        
     
+    ///@author Andrew
+    ///Adds items with the best ratio first
     public CargoBay loadBestRatio()
     {
         CargoBay ratioBay = new CargoBay(700);
+        
+        ArrayList<Payload> dupCatalogue = new ArrayList<>(cargoCatalogue);
+
+        while (true) {
+            Payload bestRatio = null;
+            
+            //loops through catalogue to find item with the max ratio
+            for (int i = 0; i < dupCatalogue.size(); i++) {
+                Payload current = dupCatalogue.get(i);
+             //checks if weight keeps total under 700 and updates bestRatio if needed
+                if (current.getWeight() + ratioBay.getCargoWeight() <= 700) {
+                    if (bestRatio == null || current.getRatio() > bestRatio.getRatio()) {
+                        bestRatio = current;
+                    }
+                }
+            }
+
+            if (bestRatio == null) {
+                break;
+            }
+            //Prevents exception error since addPayload throws exception
+            try {
+                ratioBay.addPayload(bestRatio);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            dupCatalogue.remove(bestRatio);
+        }
         
         return ratioBay;
     }
